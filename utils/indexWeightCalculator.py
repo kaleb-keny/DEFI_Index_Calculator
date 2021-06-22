56cd0ff029bc254f7f6b263205375880dd8bfd09
kaleb-keny <62237347+kaleb-keny@users.noreply.github.com>
2021-06-22 21:15:17 +0200

    Defi Price Adjustment
    - Including the extraction of existing defi price
    - Transformation of units to ensure continuity of defi price when switch to new synths

--- utils/indexWeightCalculator.py
+++ utils/indexWeightCalculator.py
@@ -23,7 +23,28 @@ class calculator(geckoAPI):
         df["value"] = df["concentration"] * 1000
         df["units"] = df["value"] / df["price"]
         
+        #get current defi index price
+        previousDefiPrice = self.getDefiIndexPrice()
+        print(f"existing sDEFI price is {previousDefiPrice}")
+
+
+        #compute denominator that keeps the price constant
+        denominator =  df["units"].multiply(df["price"]).sum() / previousDefiPrice
+        print(f"denominator used is {denominator}")
+                
+        df["units_den_adjusted"] = df["units"] / denominator
+        
+        newDefiPrice = df["units_den_adjusted"].multiply(df["price"]).sum()
+        print(f"new sDEFI price is {newDefiPrice}")
+
         df.to_csv(r'output\output.csv')
+            
+        
+    def getDefiIndexPrice(self):
+        df = pd.read_html('https://www.synthetix-monitoring.com/')[0]
+        df = df.query('Synth == "sDEFI"').copy()
+        return df.Price.values[0]
+        
                 
 #%%
 if __name__ == '__main__':
